0f705ffb8840fea5835586a9494b14c973f495ca
solkatt <viktoralexanderodman@gmail.com>
2023-11-30 23:08:31 +0100

    Update Plane: add shadowmaterial

--- app/components/r3f/Plane.tsx
+++ app/components/r3f/Plane.tsx
@@ -3,8 +3,10 @@ const Plane = (props: any) => {
 
     return (
         <mesh {...props} receiveShadow>
-            <planeGeometry args={[1, 1, 1]} />
-            <meshPhysicalMaterial attach="material" color="white" />
+            <planeGeometry args={[2, 1.5, 1]} />
+            <shadowMaterial attach="material" transparent opacity={0.4} />
+
+            {/* <meshPhysicalMaterial attach="material" color="white" /> */}
         </mesh>
     )
 }
