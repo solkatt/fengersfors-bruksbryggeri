ada7af2f31b8c547bdecedb4686cac556489618d
Viktor Ödman <viktor.odman@consid.se>
2024-06-03 19:49:43 +0200

    Update Scene: add mock camera

--- app/components/r3f/Scene.tsx
+++ app/components/r3f/Scene.tsx
@@ -30,192 +30,11 @@ import { useDrag, useGesture } from "@use-gesture/react";
 import Bottle from "./Bottle";
 import Poster from "./Poster";
 import Plane from "./Plane";
+import Carousell from "./Carousell";
+import { config } from "process";
+import { Environment } from "@react-three/drei";
 
 extend({ OrbitControls });
-
-const Group = ({
-  items,
-  rotation,
-  rotateL,
-  rotateR,
-  showRoom,
-  position
-}: {
-  rotation: { rotation: number; step: number };
-  items: { color: string }[];
-  rotateL: () => void;
-  rotateR: () => void;
-  showRoom: boolean;
-  position: any;
-}) => {
-  const groupRef = useRef(null);
-
-  const { rotationD } = useSpring({
-    // rotationD: rotation === 1 ? Math.PI / 2 : 0,
-    rotationD: rotation.rotation,
-    config: {
-      mass: 2,
-      friction: 50,
-      tension: 70,
-    },
-  });
-
-  // todo
-  // calculate bottle position based on number of items
-  // map bottles with correct position in a circle
-
-// -.....----
-const {viewport} = useThree()
-
-const isMobile = window.innerWidth < 768;
-
-console.log(isMobile)
-
-const responsiveRatio = viewport.width / 12;
-
-// -.....----
-
-  function getCirclePositions(nrOfItems: number, circleSize: number) {
-    const positions = [];
-    const adjustment = Math.PI / 2;
-
-    for (let i = 0; i < nrOfItems; i++) {
-      const angle = ((2 * Math.PI) / nrOfItems) * i + adjustment;
-      const x = circleSize * Math.cos(angle);
-      const y = circleSize * Math.sin(angle);
-      const angleToFaceCenter = Math.atan2(y, x);
-
-      positions.push({ x, y, angle: angleToFaceCenter });
-    }
-    return positions;
-  }
-
-  const pos = getCirclePositions(items.length, 2);
-
-  console.log("POS;", pos);
-
-  const [springs, api] = useSprings(
-    items?.length,
-    (i) => {
-      // make function for this?
-      const isActive = i === rotation.step;
-
-      const defaultPos = [pos[i].x, 0, pos[i].y];
-      const defaultRotation = [0, -pos[i].angle, 0];
-
-      // const heroPos = [pos[i].x, 3, pos[i].y];
-      const heroPos = isMobile ? [pos[i].x, 3, pos[i].y] :  [pos[i].x - responsiveRatio / 4, 3, pos[i].y] ;
-      const heroRotation = [0, -pos[i].angle + Math.PI, 0];
-
-      // const hidden = isActive ? heroPos : [pos[i].x, 4, pos[i].y];
-      const hidden = isActive ? heroPos : defaultPos;
-
-      const config = isActive
-        ? {
-            delay: 600,
-            config: {
-              mass: 2,
-              friction: 50,
-              tension: 90,
-            },
-          }
-        : {
-            delay: isActive ? 700 : i * 300,
-            config: {
-              mass: 5,
-              friction: 120,
-              tension: 120,
-            },
-          };
-
-      console.log({ defaultPos });
-      console.log("I::", i);
-      return {
-        from: { position: [pos[i].x, 4, pos[i].y], rotation: defaultRotation },
-        position: showRoom ? hidden : defaultPos,
-        rotation: showRoom && isActive ? heroRotation : defaultRotation,
-        ...config,
-        // from: { position: defaultPos },
-        // to: { position: showRoom ? [pos[i].x, 4, pos[i].y] : defaultPos },
-      };
-    },
-    [showRoom, isMobile]
-  );
-
-
-
-
-
-  // todo
-  // draggable={i === rotation.step + 2}
-  // the starting bottle should be calululated at step 0
-  // so draggable is - i === rotation.step
-
-  // TODO
-  // 3d model Bottles
-  // Shader backgrounds
-  // Transition to bottle page
-  // Transition back to carousel
-  // too much spin breaks carousel (cat noise crash)
-  // custom cursor
-  // custom loader
-  // animate in carousel and page elements
-
-  // PRODUCT PAGE
-  // animate shader poster to be a full cover bg
-  // animate non selected bottles upwards in a staggered fashion
-  // animate selected bottle to correct hero placement
-  // animate in Text and description
-  // animate in shader or coloured design elements
-
-  // turn off draggable
-  // turn on rotation of object
-  // animate in description and other fun stuff
-  //
-
-  return (
-    <animated.group rotation-y={rotationD} position={position}>
-      {/* <group ref={groupRef} rotation-y={springs.rotation.to((rotation) => rotation)} > */}
-
-      {/* <Bottle position={[1, 1, 1]} /> */}
-      {/* <mesh position={[0, 0, 0]} scale={0.3}>
-                <boxGeometry />
-                <meshStandardMaterial color="pink" />
-            </mesh> */}
-
-      {/* <Bottle color="red" position={[1, 2, 0]} /> */}
-
-      {springs.map((props, i) => {
-        console.log({ i, props });
-        return (
-          <Bottle
-            key={i}
-            draggable={i === rotation.step}
-            color={items[i].color}
-            position={props.position}
-            rotation={props.rotation}
-            //   position={[pos[i].x, 0, pos[i].y]}
-            // rotation={[0, -pos[i].angle, 0]}
-            rotateL={rotateL}
-            rotateR={rotateR}
-          />
-        );
-      })}
-
-      {/* {items.map((item, i) => (
-        <Bottle
-          key={i}
-          draggable={i === rotation.step}
-          color={item.color}
-          position={[pos[i].x, 0, pos[i].y]}
-          rotation={[0, -pos[i].angle, 0]}
-          rotateL={rotateL}
-          rotateR={rotateR}
-        />
-      ))} */}
-    </animated.group>
-  );
-};
 
 export default function Scene() {
   // const numberOfObjects = 8;
@@ -307,7 +126,6 @@ export default function Scene() {
   // function degreesToRadians(degrees: number) {
   //     return degrees * (Math.PI / 180);
   // }
-  
 
   console.log(getStepSize(4));
 
@@ -322,20 +140,6 @@ export default function Scene() {
     position: [0, 0, 5],
   };
 
-  const [props, api] = useSpring(
-    () => ({
-      position: showRoom ? [0, -3, 0.5] : [0, 0, 0],
-      rotation: showRoom ? [0, 0, 0] : [0, 0, 0], // could be rotation x only  (z/y?)
-      delay: 600,
-      config: {
-        mass: 2,
-        friction: 50,
-        tension: 90,
-      },
-    }),
-    [showRoom]
-  );
-
   return (
     // <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 1, 5] }}>
     <>
@@ -345,20 +149,20 @@ export default function Scene() {
       </button>
       <Canvas style={{ background: "white" }} shadows>
         {/* <ambientLight intensity={0.2} castShadow /> */}
-        {/* @ts-expect-error */}
-        <animated.group position={props.position} rotation={props.rotation}>
+        <Environment preset='studio' background={false}/>
+
+        <MockCamera showRoom={showRoom}>
           <directionalLight
             castShadow
             position={[2.5, 8, 5]}
             intensity={1.5}
             shadow-mapSize={2048}
           />
-
           <pointLight position={[2, 2, 6]} intensity={100} />
           {/* <Box position={[-1.2, 0, 0]} />
             <Box position={[1.2, 0, 0]} /> */}
           {/* <Group rotation-y={rotation} /> */}
-          <Group
+          <Carousell
             position={[0, 0, 2]}
             rotation={rotation}
             showRoom={showRoom}
@@ -377,10 +181,11 @@ export default function Scene() {
             rotation-x={-90 * (Math.PI / 180)}
             scale={4}
           />
-        </animated.group>
+        </MockCamera>
       </Canvas>
 
-      <Text text='Description' className={`bg-white translate-y-60  p-2 text-sm w-96 max-w-xl rounded h-40 ${showRoom ? 'opacity-100 transition-all duration-1000 delay-700 scale-100' : ' -top-[45vh]  opacity-100 transition-all duration-1000 delay-500 scale-0'}`} />
+      {/* <Description showRoom={showRoom} /> */}
+
       <div className='flex gap-4'>
         <h1>{rotation.step}</h1>
         <div
@@ -407,5 +212,102 @@ const Text = ({ text, className }: { text: string; className?: string }) => {
     >
       <h1 className=' text-orange-300 font-bold'>{text}</h1>
     </div>
+  );
+};
+
+const MockCamera = ({
+  showRoom,
+  children,
+}: {
+  showRoom: boolean;
+  children: any;
+}) => {
+  // move group instead of camera
+
+  const { viewport } = useThree();
+
+  const [test, setTest] = useState(1);
+
+  const isMobile = window.innerWidth < 768;
+  const responsiveRatio = viewport.width / 12;
+
+  const [props, api] = useSpring(
+    () => ({
+      // position: showRoom ? [0, -3, 0.5] : [0, 0, 0],
+      rotation: showRoom ? [0, 0, 0] : [0, 0, 0], // could be rotation x only  (z/y?)
+      from: { position: [0, 0, 0] },
+      // to: showRoom
+      //   ? [{ position: [0, -3, 0.5] }, { position: [-0.3, -3, 0.5] }]
+      //   : { position: [0, 0, 0] },
+      // loop: true,
+      delay: 600,
+      config: {
+        mass: 2,
+        friction: 50,
+        tension: 90,
+      },
+    }),
+    [showRoom]
+  );
+
+  const testa = () => {
+    api.start({
+      to: [
+        {
+          position: [0, -3, 0.5],
+          config: { friction: 50, tension: 90 },
+        },
+        // { position: [-0.2, -3, 0.5] },
+      ],
+      delay: 600,
+    });
+  };
+
+  const testa2 = () => {
+    api.start({
+      to: [{ position: [0, 0, 0], config: { friction: 50, tension: 90 } }],
+      delay: 600,
+    });
+  };
+
+  useEffect(() => {
+    if (showRoom) return testa();
+    testa2();
+  }, [showRoom]);
+
+  return (
+    // @ts-ignore: Spring type is Vector3 Type (Typescript return error on position)
+    <animated.group position={props.position} rotation={props.rotation}>
+      {children}
+    </animated.group>
+  );
+};
+
+const Description = ({ showRoom }: { showRoom: boolean }) => {
+  const showRoomStyle = `z-10 absolute top-1/2 left-1/2  -translate-y-1/2
+text-3xl transition-all text-red-200 duration-[3000ms] delay-[3300ms] rounded-xl
+ h-1/2 w-96
+`;
+
+  const defaultStyle = `z-10 absolute top-1/2 left-[100rem] -translate-y-1/2
+text-6xl transition-all text-red-200 duration-[1000ms] h-1/2 w-96`;
+
+  return (
+    <>
+      <div className='duration-[1000] w-1/2 dela'></div>
+      <div className={showRoom ? showRoomStyle : defaultStyle}>
+        <div className='w-full h-32 bg-white rounded-xl p-2'>Description</div>
+        <div className='flex justify-between mt-8'>
+          {["1", "2", "3"].map((item) => (
+            <div
+              key={item}
+              className='bg-white rounded-full h-24 w-24 flex items-center justify-center'
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
